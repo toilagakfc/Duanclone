cdce4ff24d9b166d1d5900b21abc9f16f860c9ee
Phong Tran <phong11223317@gmail.com>
2024-04-14 16:44:46 +0700

    learn django

---
+++ computershopping/shop/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    
+
+    def __str__(self):
+        return self.name
+
+class CartItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.quantity} of {self.product.name} for {self.user.username}"
